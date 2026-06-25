3651408971d52ce8c2038c6918545c0a59ec93e5
Farhan <farhan@gmail.com>
2026-06-25 23:38:35 +0700

    fix: hardcode default mysql database name to railway

--- backend/config/database.php
+++ backend/config/database.php
@@ -44,12 +44,12 @@ return [
             'transaction_mode' => 'DEFERRED',
         ],
 
-        'mysql' => [
+            'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'mysql.railway.internal'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'railway'), 
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
